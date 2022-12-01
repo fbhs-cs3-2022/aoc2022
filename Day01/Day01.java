ea7105b3aa8c2b8cadf8a87961437d3434deddc3
Chad Purdy <cpurdy@flourbluffschools.net>
2022-12-01 14:17:23 -0600

    part 1 working

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Day01 {
 
+    private final static String DATA_FILE = "input.txt";
+
     public static void part1() {
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch(Exception e) {
+            System.out.println(e);
+            return;
+        }
+        int mostCalories = 0;
+
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            int calories = 0;
+
+            // get total calories not separated by a space
+            while(!line.equals("") && in.hasNext()) {
+                calories += Integer.parseInt(line);
+                line = in.nextLine();
+            }
+
+            if(calories > mostCalories) {
+                mostCalories = calories;
+            }
+        }
+
+        System.out.println(mostCalories);
+
+
 
     }
 
@@ -9,6 +41,8 @@ public class Day01 {
         
     }
 
-
+    public static void main(String[] args) {
+        part1();
+    }
 
 }
