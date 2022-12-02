3a0e84c3072b99cb33f5c780b74e5021278a2561
Chad Purdy <cpurdy@flourbluffschools.net>
2022-12-02 13:53:12 -0600

    part 2 complete

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.io.File;
 
 public class Day01 {
@@ -9,7 +11,7 @@ public class Day01 {
         Scanner in;
         try {
             in = new Scanner(new File(DATA_FILE));
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
             return;
         }
@@ -20,29 +22,56 @@ public class Day01 {
             int calories = 0;
 
             // get total calories not separated by a space
-            while(!line.equals("") && in.hasNext()) {
+            while (!line.equals("") && in.hasNext()) {
                 calories += Integer.parseInt(line);
                 line = in.nextLine();
             }
 
-            if(calories > mostCalories) {
+            if (calories > mostCalories) {
                 mostCalories = calories;
             }
         }
 
         System.out.println(mostCalories);
 
-
-
     }
 
-
     public static void part2() {
-        
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+        ArrayList<Integer> calorieList = new ArrayList<Integer>();
+
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            int calories = 0;
+
+            // get total calories not separated by a space
+            while (!line.equals("") ) {
+                calories += Integer.parseInt(line);
+                if (in.hasNext()) {
+                    line = in.nextLine();
+                } else {
+                    break;
+                }
+            }
+
+            calorieList.add(calories);
+
+        }
+        Collections.sort(calorieList);
+        Collections.reverse(calorieList);
+       // System.out.println(calorieList);
+        System.out.println(calorieList.remove(0) + calorieList.remove(0) + calorieList.remove(0));
+
     }
 
     public static void main(String[] args) {
-        part1();
+        part2();
     }
 
 }
