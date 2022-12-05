b6e03b602fd3e891fcaa7e125d49a0d720cbfcf7
Chad Purdy <cpurdy@flourbluffschools.net>
2022-12-05 14:48:17 -0600

    day3 part 2 complete

--- Day03/Day03.java
+++ Day03/Day03.java
@@ -37,12 +37,30 @@ public class Day03 {
     }
 
     public static void part2() throws IOException {
+        int total = 0;
+        Scanner in = new Scanner(new File("input.txt"));
+        while(in.hasNext()) {
+            String[] rucksacks = new String[3];
+            for(int i = 0; i < 3; i++) {
+                rucksacks[i] = in.nextLine();
+            }
+            
+            for(int i = 0; i < rucksacks[0].length(); i++) {
+                String curr = rucksacks[0].substring(i,i+1);
+                if(rucksacks[1].indexOf(curr)!=-1 && rucksacks[2].indexOf(curr)!=-1) {
+                    total += decode(curr);
+                    break;
+                }
+            }
 
+
+        }
+        System.out.println(total);
     }
 
     public static void main(String...args) {
         try {
-            part1();
+            part2();
         } catch (Exception e) {
             System.out.println(e);
         }
