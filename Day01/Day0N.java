90a24a2face1ff717d7f0e0a80ff52c5f4136fca
Chad Purdy <cpurdy@flourbluffschools.net>
2022-12-05 12:45:04 -0600

    add general solution template

---
+++ Day01/Day0N.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Day0N {
+
+    public static void part1() throws IOException {
+
+    }
+
+    public static void part2() throws IOException {
+
+    }
+
+    public static void main(String...args) {
+        try {
+            part1();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
