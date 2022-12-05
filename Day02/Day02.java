b169382d07566a1e2496343c56c7b6f94ebaae3f
Chad Purdy <cpurdy@flourbluffschools.net>
2022-12-05 14:03:33 -0600

    part 2 complete

--- Day02/Day02.java
+++ Day02/Day02.java
@@ -21,6 +21,20 @@ public class Day02 {
         return score;
     }
 
+    public static int evaluate2(String strategy) {
+        int score = 0;
+        String[] choices = strategy.split(" ");
+        int otherChoice = choices[0].compareTo("A");
+        int result = choices[1].compareTo("X");
+        //System.out.println(diffMod3);
+        switch(result) {
+            case 0: score += Math.floorMod(otherChoice - 1,3)+1; break;
+            case 1: score += (otherChoice + 1)+3; break;
+            case 2: score += Math.floorMod(otherChoice + 1,3)+6+1; break;
+        }
+        return score;
+    }
+
     public static void part1() throws IOException {
         Scanner in = new Scanner(new File("input.txt"));
         int total = 0;
@@ -32,13 +46,20 @@ public class Day02 {
         System.out.println(total);
     }
 
-    public static void part2() {
-
+    public static void part2() throws IOException{
+        Scanner in = new Scanner(new File("input.txt"));
+        int total = 0;
+        while(in.hasNext()) {
+            String line = in.nextLine();
+           //System.out.println(line);
+            total += evaluate2(line);
+        }
+        System.out.println(total);
     }
 
     public static void main(String...args) {
         try {
-            part1();
+            part2();
         } catch (Exception e) {
             System.out.println(e);
         }
