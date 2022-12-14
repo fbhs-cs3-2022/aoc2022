3d3c95661d5b60dfb9067e870344bfb60da298a3
Chad Purdy <cpurdy@flourbluffschools.net>
2022-12-14 08:31:50 -0600

    part 2 solved

--- Day08/Day08.java
+++ Day08/Day08.java
@@ -1,72 +1,68 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Day08 {
-
-    private static lcm = 1;
 
     private static class Tree {
         boolean canSee;
         int height;
-
+        int score;
 
         public Tree(int h, boolean canSee) {
             height = h;
             this.canSee = canSee;
+            score = 0;
         }
 
-        public int getScore(Tree[][] trees, int r, int c) {
-            int up = 0;
-            int left = 0;
-            int right = 0;
-            int down = 0;
+        public void getScore(Tree[][] trees, int r, int c) {
 
-            if(r > 0) {
-                up++;
-                for(int row = r-1; row > 0; row--) {
-                    if(trees[row][c].height < height) {
-                        up++;
-                    } else {
-                        break;
-                    }
+            if (r == 0 || c == 0 || r == trees.length - 1 || c == trees[0].length - 1) {
+                score = 0;
+                return;
+            }
+
+            int up = 1;
+            int left = 1;
+            int right = 1;
+            int down = 1;
+
+            // up
+            for (int row = r - 1; row > 0; row--) {
+                if (trees[row][c].height < height) {
+                    up++;
+                } else {
+                    break;
                 }
             }
 
-            if(r < trees.length-1) {
-                down++;
-                for(int row=r+1; row < trees.length-1;row++) {
-                    if(trees[row][c].height < height) {
-                        down++;
-                    } else {
-                        break;
-                    }
+            // down
+            for (int row = r + 1; row < trees.length - 1; row++) {
+                if (trees[row][c].height < height) {
+                    down++;
+                } else {
+                    break;
                 }
             }
 
-            if(c < trees[0].length-1) {
-                right++;
-                for(int col=c+1; col < trees[0].length-1;col++) {
-                    if(trees[r][col].height < height) {
-                        right++;
-                    } else {
-                        break;
-                    }
+            // right
+            for (int col = c + 1; col < trees[0].length - 1; col++) {
+                if (trees[r][col].height < height) {
+                    right++;
+                } else {
+                    break;
                 }
             }
 
-            if(c > 0) {
-                left++;
-                for(int col=c-1; col > 0;col++) {
-                    if(trees[r][col].height < height) {
-                        left++;
-                    } else {
-                        break;
-                    }
+            // left
+            for (int col = c - 1; col > 0; col--) {
+                if (trees[r][col].height < height) {
+                    left++;
+                } else {
+                    break;
                 }
             }
 
-            return up * left * right * down;
+            score = up * left * right * down;
         }
 
         public String toString() {
@@ -74,78 +70,76 @@ public class Day08 {
         }
     }
 
-
     public static void checkVisible(Tree[][] trees) {
-        //System.out.println("Checking...");
-        for(int r = 0; r < trees.length; r++) {
-            for(int c = 0; c < trees[0].length; c++) {
-                //System.out.println(r + " " + c);
-                if(trees[r][c].canSee)
+        // System.out.println("Checking...");
+        for (int r = 0; r < trees.length; r++) {
+            for (int c = 0; c < trees[0].length; c++) {
+                // System.out.println(r + " " + c);
+                if (trees[r][c].canSee)
                     continue;
-                
+
                 Tree thisTree = trees[r][c];
-                
+
                 // check from top
                 boolean isVisible = true;
-                for(int row=0; row < r; row++) {
-                    
-                    if(trees[row][c].height >= thisTree.height) {
-                        
+                for (int row = 0; row < r; row++) {
+
+                    if (trees[row][c].height >= thisTree.height) {
+
                         isVisible = false;
                         break;
                     }
                 }
-                if(isVisible) {
+                if (isVisible) {
                     thisTree.canSee = true;
                     continue;
                 }
-                
+
                 // check from bottom
                 isVisible = true;
-                for(int row=trees.length-1; row > r; row--) {
-                    
-                    if(trees[row][c].height >= thisTree.height) {
+                for (int row = trees.length - 1; row > r; row--) {
+
+                    if (trees[row][c].height >= thisTree.height) {
                         isVisible = false;
                         break;
                     }
                 }
-                if(isVisible) {
+                if (isVisible) {
                     thisTree.canSee = true;
                     continue;
                 }
 
                 // check from left
                 isVisible = true;
-                for(int col=0; col < c; col++) {
-                    if(trees[r][col].height >= thisTree.height) {
+                for (int col = 0; col < c; col++) {
+                    if (trees[r][col].height >= thisTree.height) {
                         isVisible = false;
                         break;
                     }
                 }
-                if(isVisible) {
+                if (isVisible) {
                     thisTree.canSee = true;
                     continue;
                 }
 
                 // check from right
                 isVisible = true;
-                for(int col=trees[r].length-1; col > c; col--) {
-                    
-                    if(trees[r][col].height >= thisTree.height) {
+                for (int col = trees[r].length - 1; col > c; col--) {
+
+                    if (trees[r][col].height >= thisTree.height) {
                         isVisible = false;
                         break;
                     }
                 }
-                if(isVisible) {
+                if (isVisible) {
                     thisTree.canSee = true;
                     continue;
                 }
 
             }
-            
+
         }
         System.out.println("Done");
-       
 
     }
 
@@ -154,7 +148,7 @@ public class Day08 {
 
         ArrayList<String> rawTrees = new ArrayList<String>();
 
-        while(in.hasNext()) {
+        while (in.hasNext()) {
             rawTrees.add(in.nextLine());
         }
         System.out.println(rawTrees);
@@ -164,36 +158,35 @@ public class Day08 {
 
         Tree[][] trees = new Tree[height][width];
 
-        for(int i = 0; i < rawTrees.size();i++) {
-            for(int j = 0; j < rawTrees.get(i).length();j++) {
-                if(i == 0 || j == 0 || i == rawTrees.size()-1 || j == rawTrees.get(i).length()-1)
-                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j,j+1)), true);
+        for (int i = 0; i < rawTrees.size(); i++) {
+            for (int j = 0; j < rawTrees.get(i).length(); j++) {
+                if (i == 0 || j == 0 || i == rawTrees.size() - 1 || j == rawTrees.get(i).length() - 1)
+                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j, j + 1)), true);
                 else
-                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j,j+1)), false);
+                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j, j + 1)), false);
             }
         }
 
         // edges
-        for(Tree[] row: trees) {
+        for (Tree[] row : trees) {
             for (Tree t : row) {
                 System.out.print(t);
             }
             System.out.println();
-           }
-
-       checkVisible(trees);
-
-       int numVisible = 0;
-
-       for(Tree[] row: trees) {
-        for (Tree t : row) {
-            if(t.canSee)
-                numVisible++;
         }
-       }
 
-       
-       System.out.println(numVisible);
+        checkVisible(trees);
+
+        int numVisible = 0;
+
+        for (Tree[] row : trees) {
+            for (Tree t : row) {
+                if (t.canSee)
+                    numVisible++;
+            }
+        }
+
+        System.out.println(numVisible);
     }
 
     public static void part2() throws IOException {
@@ -201,61 +194,71 @@ public class Day08 {
 
         ArrayList<String> rawTrees = new ArrayList<String>();
 
-        while(in.hasNext()) {
+        while (in.hasNext()) {
             rawTrees.add(in.nextLine());
         }
-        System.out.println(rawTrees);
 
         int width = rawTrees.get(0).length();
         int height = rawTrees.size();
 
         Tree[][] trees = new Tree[height][width];
 
-        for(int i = 0; i < rawTrees.size();i++) {
-            for(int j = 0; j < rawTrees.get(i).length();j++) {
-                if(i == 0 || j == 0 || i == rawTrees.size()-1 || j == rawTrees.get(i).length()-1)
-                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j,j+1)), true);
+        for (int i = 0; i < rawTrees.size(); i++) {
+            for (int j = 0; j < rawTrees.get(i).length(); j++) {
+                if (i == 0 || j == 0 || i == rawTrees.size() - 1 || j == rawTrees.get(i).length() - 1)
+                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j, j + 1)), true);
                 else
-                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j,j+1)), false);
+                    trees[i][j] = new Tree(Integer.parseInt(rawTrees.get(i).substring(j, j + 1)), false);
             }
         }
 
         // edges
-        for(Tree[] row: trees) {
+        for (Tree[] row : trees) {
             for (Tree t : row) {
                 System.out.print(t);
             }
             System.out.println();
-           }
-
-       for(int r = 0; r < trees.length; r++) {
-        for(int c = 0; c< trees[0].length; c++) {
-            trees[r][c].getScore(trees,r,c);
         }
-       }
 
-       int numVisible = 0;
-
-       for(Tree[] row: trees) {
-        for (Tree t : row) {
-            if(t.canSee)
-                numVisible++;
+        for (int r = 0; r < trees.length; r++) {
+            for (int c = 0; c < trees[0].length; c++) {
+                trees[r][c].getScore(trees, r, c);
+            }
         }
-       }
 
-       
-       System.out.println(numVisible);
+        int maxScore = 0;
+
+        for (Tree[] row : trees) {
+            for (Tree t : row) {
+                if (t.score > maxScore) {
+                    maxScore = t.score;
+                }
+            }
+        }
+        System.out.println(maxScore);
+
+        int largest = 0;
+        for (int r = 0; r < trees.length; r++) {
+            for (int c = 0; c < trees[0].length; c++) {
+                int up = r;
+                int down = trees.length - r - 1;
+                int left = c;
+                int right = trees[0].length - c - 1;
+                if (largest < up * down * left * right) {
+                    largest = up * down * left * right;
+                }
+            }
+        }
+        System.out.println(largest);
+
     }
 
-    }
-
-    public static void main(String...args) {
+    public static void main(String... args) {
         try {
             part2();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-
 
 }
